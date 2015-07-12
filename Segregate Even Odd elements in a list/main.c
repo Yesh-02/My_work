83a42eb26efafe737a1f4c0959a6a3c30cf23558
Yeshwanth D S <yeshhwanth93@gmail.com>
2015-07-13 01:32:04 +0530

    Segregate even and odd numbers in a list

--- Segregate Even Odd elements in a list/main.c
+++ Segregate Even Odd elements in a list/main.c
@@ -40,18 +40,11 @@ void segregateEvenOdd(struct list * head)
 }
 void push(struct list** head_ref, int new_data)
 {
-    /* allocate node */
     struct list* new_node =
         (struct list*) malloc(sizeof(struct list));
- 
-    /* put in the data  */
-    new_node->data  = new_data;
- 
-    /* link the old list off the new node */
-    new_node->next = (*head_ref);
- 
-    /* move the head to point to the new node */
-    (*head_ref)    = new_node;
+     new_node->data  = new_data;
+     new_node->next = (*head_ref);
+     (*head_ref)    = new_node;
 }
 int main()
 {
