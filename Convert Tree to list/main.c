de13d9ec59404406b3b2e2d02bcf0cc644caab08
Yeshwanth D S <yeshhwanth93@gmail.com>
2015-07-13 01:49:59 +0530

    Convert Tree to list

--- Convert Tree to list/main.c
+++ Convert Tree to list/main.c
@@ -1,86 +1,46 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
-
-/* The node type from which both the tree and list are built */
 struct node {
     int data;
     struct node* left;
     struct node* right;
 };
 typedef struct node* Node;
-
-
-
-/*
- helper function -- given two list nodes, join them
- together so the second immediately follow the first.
- Sets the .next of the first and the .previous of the second.
- */
 static void join(Node a, Node b) {
     a->right = b;
     b->left = a;
 }
-
-
-/*
- helper function -- given two circular doubly linked
- lists, append them and return the new list.
- */
 static Node append(Node a, Node b) {
     Node aLast, bLast;
-    
     if (a==NULL) return(b);
     if (b==NULL) return(a);
-    
     aLast = a->left;
     bLast = b->left;
-    
     join(aLast, b);
     join(bLast, a);
-    
     return(a);
 }
-
-
-/*
- --Recursion--
- Given an ordered binary tree, recursively change it into
- a circular doubly linked list which is returned.
- */
 static Node treeToList(Node root) {
     Node aList, bList;
     
     if (root==NULL) return(NULL);
-    
-    /* recursively solve subtrees -- leap of faith! */
     aList = treeToList(root->left);
     bList = treeToList(root->right);
-    
-    /* Make a length-1 list ouf of the root */
     root->left = root;
     root->right = root;
-    
-    /* Append everything together in sorted order */
     aList = append(aList, root);
     aList = append(aList, bList);
-    
     return(aList);
-    
 }
-
-    /* Create a new node */
-    static Node newNode(int data) {
+static Node newNode(int data) {
         Node node = (Node) malloc(sizeof(struct node));
         node->data = data;
         node->left = NULL;
         node->right = NULL;
         return(node);
     }
-    
-    
-    /* Add a new node into a tree */
-    static void treeInsert(Node* rootRef, int data) {
+static void treeInsert(Node* rootRef, int data) {
         Node root = *rootRef;
         if (root == NULL) *rootRef = newNode(data);
         else {
@@ -88,11 +48,8 @@ static Node treeToList(Node root) {
             else treeInsert(&(root->right), data);
         }
     }
-    
-    
-    static void printList(Node head) {
+static void printList(Node head) {
         Node current = head;
-        
         while(current != NULL) {
             printf("%d ", current->data);
             current = current->right;
@@ -114,7 +71,6 @@ void inOrder(Node node)
     int main() {
         Node root = NULL;
         Node head;
-        
         treeInsert(&root, 4);
         treeInsert(&root, 5);
         treeInsert(&root, 2);
@@ -124,10 +80,7 @@ void inOrder(Node node)
         treeInsert(&root, 9);
         inOrder(root);
         printf("\n");
-        
         head = treeToList(root);
-        
         printList(head);    /* prints: 1 2 3 4 5  */
-        
         return(0);
     }
